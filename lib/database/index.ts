fb6d9b214c6291f2cb82cca9c92362b2f78c4ca9
Adinath Shelke <shelkeadinath3@gmail.com>
2023-12-31 23:38:32 +0530

    created user and events models

---
+++ lib/database/index.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGODB_URI;
+
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
+
+  if (!MONGO_URI) throw new Error("MONGODB_URI is missing");
+
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGO_URI, {
+      dbName: "eventhub",
+      bufferCommands: false,
+    });
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+};
