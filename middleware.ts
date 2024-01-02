576e0345c5ad986d16f6fea1c27b84763efa0efa
Adinath Shelke <shelkeadinath3@gmail.com>
2024-01-02 15:03:35 +0530

    route.js added in webhook

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-    publicRoutes:[
-        '/',
-        '/events/:id',
-        '/api/webhook/clerk',
-        '/api/webhooks(.*)',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
-    ],
-    ignoredRoutes:[
-        '/api/webhook/clerk',
-        '/api/webhook/stripe',
-        '/api/uploadthing'
-    ]
+  publicRoutes: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/webhooks(.*)",
+    "/api/webhook(.*)",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
+  ignoredRoutes: [
+    "/api/webhook/clerk",
+    "/api/webhook/stripe",
+    "/api/uploadthing",
+  ],
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
- 
