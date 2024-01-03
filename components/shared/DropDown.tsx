eef216f59d7d2e2696dbc8e0d023acbb27761b8c
Adinath Shelke <shelkeadinath3@gmail.com>
2024-01-03 14:42:01 +0530

    added select item in create event

---
+++ components/shared/DropDown.tsx
@@ -0,0 +1,40 @@
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { ICategory } from "@/lib/database/models/category.model";
+import { useState } from "react";
+type DropDownProps = {
+  value?: string;
+  onChangeHandler?: () => void;
+};
+const DropDown = ({ value, onChangeHandler }: DropDownProps) => {
+  const [categories, setcategories] = useState<ICategory[]>([
+    { _id: "1", name: "Category 1" },
+    { _id: "2", name: "Category 2" },
+  ]);
+  return (
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.length > 0 &&
+          categories.map((category) => (
+            <SelectItem
+              key={category._id}
+              value={category._id}
+              className="select-item p-regular-14"
+            >
+              {category.name}
+            </SelectItem>
+          ))}
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default DropDown;
