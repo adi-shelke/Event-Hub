ed8850a0764492100c0d5ee8f280bea471819381
Adinath Shelke <shelkeadinath3@gmail.com>
2024-02-04 01:53:06 +0530

    added edit icon

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,5 +1,6 @@
 import { IEvent } from "@/lib/database/models/event.model";
 import { formatDateTime } from "@/lib/utils";
+import { auth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -10,6 +11,9 @@ type CardProps = {
   hidePrice?: boolean;
 };
 const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+  const isEventCreator = event.organizer?._id === userId;
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
       <Link
@@ -18,6 +22,18 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         className="flex-center flex-grow bg-gray-50 bg-cover bg-no-repeat bg-center text-grey-500"
       />
       {/*IS EVENT CREATOR*/}
+      {isEventCreator && !hidePrice && (
+        <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
+          <Link href={`/events/${event._id}/update`}>
+            <Image
+              src="/assets/icons/edit.svg"
+              alt="edit"
+              height={20}
+              width={20}
+            />
+          </Link>
+        </div>
+      )}
 
       <Link
         href={`/events/${event._id}`}
